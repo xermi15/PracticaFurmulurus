ac343fb077480a6e750f1bad37ba89905577b8f4
Xavier Franch <43445674+xermi15@users.noreply.github.com>
2019-02-09 14:48:36 +0100

    Boto eliminar

--- main.js
+++ main.js
@@ -3,12 +3,32 @@ function crearFormulari() {
 	var formulari = document.createElement("form");
   var titolForm = document.createElement("h4");
   var textTitol = document.createTextNode("Formulari " + inputCrear.value);
+  formulari.setAttribute("id", inputCrear.value);
 
   titolForm.appendChild(textTitol);
   formulari.appendChild(titolForm);
+  formulari.appendChild(botoEliminar(formulari.id));
   container.appendChild(formulari);
 }
 
+function botoEliminar(idF) {
+  var boto = document.createElement("button");
+  boto.setAttribute("id", "botoEliminar");
+  boto.setAttribute("type", "button");
+  boto.setAttribute("class", "btn btn-danger");
+  var textBoto = document.createTextNode("Eliminar");
+
+  boto.appendChild(textBoto);
+
+  var botoE = boto;
+  //botoE.addEventListener("click", eliminarFormulari(idF));
+  return boto;
+}
+
+function eliminarFormulari(idF) {
+  var form = document.getElementById(idF);
+  form.remove();
+}
 
 
 

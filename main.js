a2895c6373d00ed4579c25b3982f5b919371838d
Xavier Franch <xavi.franchtarazaga@gmail.com>
2019-02-12 00:25:07 +0100

    Funcions crearInput i crearEmail

--- main.js
+++ main.js
@@ -34,7 +34,54 @@ function eliminarFormulari(idF) {
 
 
 
+function crearBoto(textBoto) {
+  var boto = document.createElement("button");
 
+  boto.setAttribute("id", "boto");
+  boto.setAttribute("type", "button");
+  boto.setAttribute("class", "btn btn-primary");
+  boto.appendChild(textBoto);
+
+  return boto;
+}
+
+function crearInput(textInput) {
+  var input = document.createElement("input");
+  var inputLabel = document.createElement("label");
+  var formGroup = document.createElement("div");
+
+  input.setAttribute("id", "input");
+  input.setAttribute("type", "input");
+  input.setAttribute("class", "form-control");
+
+  inputLabel.setAttribute("for", "input");
+  inputLabel.appendChild(textInput);
+
+  formGroup.setAttribute("class", "form-group");
+  formGroup.appendChild(inputLabel);
+  formGroup.appendChild(input);
+
+  return formGroup;
+}
+
+function crearEmail() {
+  var email = document.createElement("email");
+  var emailLabel = document.createElement("label");
+  var formGroup = document.createElement("div");
+
+  email.setAttribute("id", "input");
+  email.setAttribute("type", "input");
+  email.setAttribute("class", "form-control");
+
+  inputLabel.setAttribute("for", "input");
+  inputLabel.appendChild(document.createTextNode("Email"));
+
+  formGroup.setAttribute("class", "form-group");
+  formGroup.appendChild(inputLabel);
+  formGroup.appendChild(input);
+
+  return formGroup;
+}
 
 
 
